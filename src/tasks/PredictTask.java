a41067a54d8350a2907b74c5976976cb00197792
Pablo Barrientos <pablo.barrientos.13@gmail.com>
2014-11-10 13:57:51 +0100

    Pequeño cambio en los Tasks

--- src/tasks/PredictTask.java
+++ src/tasks/PredictTask.java
@@ -1,5 +1,31 @@
 package tasks;
 
-public class PredictTask {
+import javafx.concurrent.Task;
+import algorithms.NaiveBayes;
+import application.MainApplication;
+
+public class PredictTask extends Task<Void> {
+	
+	private final NaiveBayes alg;
+	private final MainApplication mainApplication;
+	
+	public PredictTask(NaiveBayes alg, MainApplication app){
+		this.alg = alg;
+		this.mainApplication = app;
+	}
+	
+	@Override
+	protected Void call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public NaiveBayes getAlg() {
+		return alg;
+	}
+
+	public MainApplication getMainApplication() {
+		return mainApplication;
+	}
 
 }
--- src/tasks/TrainTask.java
+++ src/tasks/TrainTask.java
@@ -2,19 +2,30 @@ package tasks;
 
 import javafx.concurrent.Task;
 import algorithms.NaiveBayes;
+import application.MainApplication;
 
 public class TrainTask extends Task<Void> {
 
 	private final NaiveBayes alg;
+	private final MainApplication mainApplication;
 	
-	public TrainTask(NaiveBayes alg){
+	public TrainTask(NaiveBayes alg, MainApplication app){
 		this.alg = alg;
+		this.mainApplication = app;
 	}
 	
 	@Override
 	protected Void call() throws Exception {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public NaiveBayes getAlg() {
+		return alg;
+	}
+
+	public MainApplication getMainApplication() {
+		return mainApplication;
 	}
 
 }
